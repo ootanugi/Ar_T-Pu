3530d64b279460d5206ad59c941dee957f0a10be
ootanugi <76213879+ootanugi@users.noreply.github.com>
2020-12-19 11:52:55 +0700

    TestAnim

---
+++ AR/Assets/Script/Anim.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Anim : MonoBehaviour
+{
+
+
+    private void Awake()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        transform.DOScale(1,Random.Range(0.3f,0.5f)).SetEase(Ease.OutBack);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
