a07b1082d1ace61c27dd8d087ae7ab3de4f5ce2b
ootanugi <76213879+ootanugi@users.noreply.github.com>
2020-12-29 10:20:54 +0700

    sound

---
+++ AR/Assets/Script/AudioSr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSr : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip ButtonSound;
+    private AudioSource AuSr;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AuSr = GetComponent<AudioSource>();
+    }
+
+    public void PlayButtonSound()
+    {
+        AuSr.PlayOneShot(ButtonSound);
+    }
+}
