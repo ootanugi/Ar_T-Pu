69ababd2fd51c171afaccdc2db833c2d8614ba08
ootanugi <76213879+ootanugi@users.noreply.github.com>
2021-01-08 21:55:41 +0700

    How to

--- AR_IOS/Assets/Script/CameraTest.cs
+++ AR_IOS/Assets/Script/CameraTest.cs
@@ -13,8 +13,9 @@ public class CameraTest : MonoBehaviour
 {
     GameObject dialog = null;
 
-    public GameObject Img1, Img2;
-    public Image Fade_Img;
+    public GameObject Img1, Img2, HowtoPopup;
+    public Image Fade_Img, howtoB;
+    
 
     void Update()
     {
@@ -34,9 +35,24 @@ public class CameraTest : MonoBehaviour
     public void Next_Scene(Image T)
     {
         T.DOColor(new Color(1, 1, 1, 0), 1).OnComplete(() => AnimBG()) ;
+        howtoB.DOColor(new Color(1, 1, 1, 0), 1);
     }
 
-    public void AnimBG()
+    public void How_To_Button()
+    {
+        HowtoPopup.SetActive(true);
+        HowtoPopup.transform.DOLocalMoveY(0, 1).SetEase(Ease.OutBack);
+    }
+
+    public void Close_Button()
+    {
+        HowtoPopup.transform.DOLocalMoveY(-1500, 1).SetEase(Ease.InBack).OnComplete(() => HowtoPopup.SetActive(false));
+    }
+
+
+
+
+        public void AnimBG()
     {
         Img1.transform.DOMoveY(60, 1).SetEase(Ease.InBack);
         Img2.transform.DOMoveY(600, 1).SetEase(Ease.InBack).OnComplete(() => Fade_Img.DOColor(new Color(1, .9f, .7f, 1), 1).OnComplete(() => SceneManager.LoadScene(1))) ;
